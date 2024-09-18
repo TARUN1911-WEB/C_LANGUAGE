7cdbea9bab0561fe709d7e087a0f1dae23ef0fe0
TARUN1911-WEB <goswamitarun1911@gmail.com>
2024-09-18 11:27:07 +0530

    lecture data

---
+++ custompattern.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+//Custom Pattern 
+
+
+main(){
+	
+	int i, j;
+	
+//	for(i=1; i<=5; i++){
+//		for(j=1; j<=i; j++){
+////			printf("%d", j);
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	for(i=1; i<=5; i++){
+//		for(j=5; j>=i; j--){
+////			printf("%d", j);
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	
+	
+	
+	
+//	*
+//	**
+//	***
+//	****
+//	*****
+//	*****
+//	****
+//	***
+//	**
+//	*
+
+
+for(i=1; i<=5; i++){
+		for(j=1; j<=i; j++){
+//			printf("%d", j);
+			printf("*");
+		}
+		printf("\n");
+	}
+	for(i=4; i>=1; i--){
+		for(j=1; j<=i; j++){
+//			printf("%d", j);
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	
+	
+	
+}
